e9b78b692b2cf1c34f4a7fceb2cd6cd10460e7c6
BenWorley-06 <ben.e.worley06@gmail.com>
2025-05-23 20:44:25 -0700

    made interactables class

---
+++ tinyware_2025/Game Objects/interactables/interactable.gd
@@ -0,0 +1,9 @@
+extends Area2D
+class_name Interactable
+
+@export var stats: Interactable_Stats
+
+var can_interact = false
+
+func interact() -> void:
+	pass
---
+++ tinyware_2025/Game Objects/interactables/interactable_stats.gd
@@ -0,0 +1,4 @@
+extends Resource
+class_name Interactable_Stats
+
+@export var interact_range: float = 500
